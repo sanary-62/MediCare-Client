89d4a4ee1747f27dd250518d160fb8c625dff8d7
sanary-62 <uzmanisafat@gmail.com>
2025-07-27 18:06:57 +0600

    added final code

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:5000/`
+    baseURL: `https://medicare-server-8b0q3y0hd-sanary-62s-projects.vercel.app/`
 });
 
 const useAxios = () => {
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,60 +1,3 @@
-// import axios from 'axios';
-// import useAuth from './useAuth';
-// import { useNavigate } from 'react-router';
-
-// const axiosSecure = axios.create({
-//   baseURL: `http://localhost:5000/`,
-// });
-
-// const useAxiosSecure = () => {
-//   const { user, logout } = useAuth();
-//   const navigate = useNavigate();
-
-//   axiosSecure.interceptors.request.use(
-//     (config) => {
-//       if (user?.accessToken) {
-//         config.headers.Authorization = `Bearer ${user.accessToken}`;
-//       }
-//       return config;
-//     },
-//     (error) => {
-//       return Promise.reject(error);
-//     }
-//   );
-
-//   axiosSecure.interceptors.response.use(
-//   (res) => {
-//     return res;
-//   },
-//   (error) => {
-//     console.log('inside res interceptors ', error.response?.status);
-//     const status = error.response?.status;
-//     if (status === 403) {
-//       navigate('/dashboard');  
-//     } else if (status === 401) {
-//       logout()
-//         .then(() => {
-//           navigate('/login');
-//         })
-//         .catch(() => {});
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-
-
-//   return axiosSecure;
-// };
-
-// export default useAxiosSecure;
-
-
-
-
-
-
-
 
 import axios from 'axios';
 import useAuth from './useAuth';
@@ -62,7 +5,7 @@ import { useNavigate } from 'react-router';
 import { getIdToken } from 'firebase/auth';
 
 const axiosSecure = axios.create({
-  baseURL: `http://localhost:5000/`,
+  baseURL: `https://medicare-server-8b0q3y0hd-sanary-62s-projects.vercel.app/`,
 });
 
 const useAxiosSecure = () => {
