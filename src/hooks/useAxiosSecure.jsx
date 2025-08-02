29dcccb98d9a876fd521d446cbbe4104c980e60d
sanary-62 <uzmanisafat@gmail.com>
2025-08-02 12:52:33 +0600

    added rechecking code

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router';
 import { getIdToken } from 'firebase/auth';
 
 const axiosSecure = axios.create({
-  baseURL: `https://medicare-server-8b0q3y0hd-sanary-62s-projects.vercel.app/`,
+  baseURL: `https://medicare-server-eight.vercel.app`,
 });
 
 const useAxiosSecure = () => {
